4f96ef66eb4851668a25d70bfb7f0aa481f23925
Koji NAKAMURA <kozy4324@gmail.com>
2026-01-24 00:43:32 +0900

    Add test case

--- test/rbs/test_patch.rb
+++ test/rbs/test_patch.rb
@@ -295,5 +295,41 @@ module RBS
         end
       EXPECTED
     end
+
+    def test_inserts_class_before_specific_class
+      p = RBS::Patch.new
+      p.apply(<<~RBS)
+        class A
+          def a: () -> void
+        end
+        class B
+          def b: () -> void
+        end
+        class C
+          def c: () -> void
+        end
+      RBS
+      p.apply(<<~RBS)
+        %a{patch:prepend_before:A}
+        class D
+          def d: () -> void
+        end
+      RBS
+
+      assert_equal(<<~EXPECTED, p.to_s)
+        class D
+          def d: () -> void
+        end
+        class A
+          def a: () -> void
+        end
+        class B
+          def b: () -> void
+        end
+        class C
+          def c: () -> void
+        end
+      EXPECTED
+    end
   end
 end
