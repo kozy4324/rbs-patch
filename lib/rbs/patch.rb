9b196aaaddc2dde43a6397b5922492698d00fdb9
Koji NAKAMURA <kozy4324@gmail.com>
2026-01-25 14:37:22 +0900

    complete rewriting

--- lib/rbs/patch.rb
+++ lib/rbs/patch.rb
@@ -12,8 +12,14 @@ module RBS
     ANNOTATION_PREPEND_BEFORE = /\Apatch:prepend_before:(.*)\Z/
 
     def initialize
-      @env = ::RBS::Environment.new
       @decls = []
+    end
+
+    def to_s
+      io = ::StringIO.new
+      RBS::Writer.new(out: io).write(@decls)
+      io.rewind
+      io.read
     end
 
     def apply(source = nil, path: nil)
@@ -24,73 +30,6 @@ module RBS
 
           files << path
           apply Buffer.new(name: path, content: path.read(encoding: "UTF-8"))
-        end
-        return
-      end
-
-      _, dirs, decls = ::RBS::Parser.parse_signature(source)
-      @env.add_source(::RBS::Source::RBS.new(source, dirs, decls))
-
-      process_class_decls @env.class_decls
-
-      @env.class_decls.each_value.map do |class_entry|
-        next if process_context_decls(class_entry.context_decls)
-
-        class_entry.context_decls.map { _2 }.inject do |decl_a, decl_b|
-          process_members(decl_a.members, decl_b.members)
-          decl_a
-        end
-      end
-    end
-
-    def to_s
-      unless @decls.empty?
-        io = ::StringIO.new
-        RBS::Writer.new(out: io).write(@decls)
-        io.rewind
-        return io.read
-      end
-
-      decls = @env.class_decls.each_value.map do |class_entry|
-        decls = class_entry.context_decls.map { _2 }
-        next if decls.empty?
-
-        decls.each_with_object(decls.first.update(members: [])) do |decl, new_decl|
-          # merge multiple class decls into a single one
-          new_decl.members.concat decl.members
-        end
-      end.compact
-
-      classes = Set[]
-      decls.each do |decl|
-        decl.members.each do |member|
-          classes << member.name if member.respond_to?(:name)
-        end
-      end
-      decls.delete_if { |c| classes.include?(c.name) }
-
-      r = lambda { |member|
-        if member.respond_to?(:members)
-          member.members.delete_if { |m| m.respond_to?(:members) && m.members.empty? }
-          member.members.each(&r)
-        end
-      }
-      decls.each(&r)
-
-      io = ::StringIO.new
-      RBS::Writer.new(out: io).write(decls)
-      io.rewind
-      io.read
-    end
-
-    def apply2(source = nil, path: nil)
-      unless path.nil?
-        files = Set[]
-        ::RBS::FileFinder.each_file(path, skip_hidden: true) do |path|
-          next if files.include?(path)
-
-          files << path
-          apply2 Buffer.new(name: path, content: path.read(encoding: "UTF-8"))
         end
         return
       end
@@ -204,108 +143,6 @@ module RBS
         [:append_after, anno.string.match(ANNOTATION_APPEND_AFTER)[1]]
       elsif (anno = annotations.find { |a| a.string.match(ANNOTATION_PREPEND_BEFORE) })
         [:prepend_before, anno.string.match(ANNOTATION_PREPEND_BEFORE)[1]]
-      end
-    end
-
-    def process_members(members_a, members_b)
-      members_b.delete_if do |member_b|
-        ope, arg = process_annotations(member_b.annotations)
-        next unless ope
-
-        case ope
-        when :override
-          index = members_a.find_index { |member_a| member_a.name == member_b.name }
-          if index
-            members_a[index] = members_a[index].update(overloads: member_b.overloads)
-            true
-          else
-            false
-          end
-        when :delete
-          members_a.reject! { |member_a| member_a.name == member_b.name }
-        when :append_after, :prepend_before
-          target_name = arg.to_sym
-          index = members_a.find_index { |member_a| member_a.name == target_name }
-          if index
-            if ope == :append_after
-              offset = 1
-              annotations = member_b.annotations.reject { |a| a.string.match(ANNOTATION_APPEND_AFTER) }
-            else
-              offset = 0
-              annotations = member_b.annotations.reject { |a| a.string.match(ANNOTATION_PREPEND_BEFORE) }
-            end
-            members_a.insert(index + offset, member_b.update(annotations:))
-            true
-          else
-            false
-          end
-        end
-      end
-    end
-
-    def process_context_decls(context_decls)
-      context_decls.reject! do |_, decl|
-        ope, = process_annotations(decl.annotations)
-        next unless ope
-
-        case ope
-        when :override
-          index = context_decls.find_index { |_, decl| decl.annotations.all? { |a| a.string != ANNOTATION_OVERRIDE } }
-          if index
-            annotations = decl.annotations.reject { |a| a.string == ANNOTATION_OVERRIDE }
-            context_decls[index] = [nil, decl.update(annotations:)]
-            true
-          else
-            false
-          end
-        when :delete
-          target_name = decl.name
-          context_decls.reject! { |_, decl| decl.name == target_name }
-        end
-      end
-    end
-
-    def process_class_decls(class_decls)
-      ope = nil
-      arg = nil
-      name, = class_decls.find do |_, class_entry|
-        class_entry.context_decls.any? do |_, decl|
-          ope, arg = process_annotations(decl.annotations)
-          %i[append_after prepend_before].include?(ope)
-        end
-      end
-      return unless name
-
-      anno_match = ->(a) { a.string.match(ANNOTATION_APPEND_AFTER) || a.string.match(ANNOTATION_PREPEND_BEFORE) }
-
-      class_to_relocate = class_decls.delete(name)
-      _, decl_to_relocate = class_to_relocate.context_decls.find { |_, decl| decl.annotations.any?(&anno_match) }
-      decl_to_relocate.annotations.delete_if(&anno_match)
-
-      target_key = RBS::TypeName.new(name: arg.to_sym, namespace: name.namespace)
-
-      return unless class_decls.key? target_key
-
-      offset = if ope == :append_after
-                 1
-               else
-                 0
-               end
-
-      ary = class_decls.to_a
-      class_decls.clear
-      index = ary.find_index { |key, _| key == target_key }
-      ary.insert(index + offset, [name, class_to_relocate])
-      ary.each { |k, v| class_decls[k] = v }
-
-      class_decls.each do |key, class_entry|
-        class_entry.context_decls.map { _2 }.each do |decl|
-          decl.members.delete(decl_to_relocate)
-          index = decl.members.find_index do |m|
-            RBS::TypeName.parse("#{key}::#{m.name}") == target_key
-          end
-          decl.members.insert(index + offset, decl_to_relocate) if index
-        end
       end
     end
   end
--- test/rbs/test_patch.rb
+++ test/rbs/test_patch.rb
@@ -6,7 +6,7 @@ module RBS
   class TestPatch < Minitest::Test
     def test_single_rbs_file_converts_to_string
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
         end
@@ -21,12 +21,12 @@ module RBS
 
     def test_merges_multiple_classes_into_single_file
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def b: () -> void
         end
@@ -42,12 +42,12 @@ module RBS
 
     def test_overrides_method_with_annotation
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           %a{patch:override}
           def a: (untyped) -> untyped
@@ -63,14 +63,14 @@ module RBS
 
     def test_override_replaces_method_at_original_position
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
           def b: () -> void
           def c: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           %a{patch:override}
           def b: (untyped) -> untyped
@@ -88,14 +88,14 @@ module RBS
 
     def test_deletes_method
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
           def b: () -> void
           def c: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           %a{patch:delete}
           def b: () -> void
@@ -113,14 +113,14 @@ module RBS
 
     def test_inserts_method_after_specific_method
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
           def b: () -> void
           def c: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           %a{patch:append_after:a}
           def d: () -> void
@@ -139,14 +139,14 @@ module RBS
 
     def test_inserts_method_before_specific_method
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
           def b: () -> void
           def c: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           %a{patch:prepend_before:a}
           def d: () -> void
@@ -165,8 +165,8 @@ module RBS
 
     def test_loads_from_file
       p = RBS::Patch.new
-      p.apply2(path: Pathname("#{__dir__}/../files/a.rbs"))
-      p.apply2(path: Pathname("#{__dir__}/../files/a_patch.rbs"))
+      p.apply(path: Pathname("#{__dir__}/../files/a.rbs"))
+      p.apply(path: Pathname("#{__dir__}/../files/a_patch.rbs"))
 
       assert_equal(<<~EXPECTED, p.to_s)
         class A
@@ -177,8 +177,8 @@ module RBS
 
     def test_loads_from_directory
       p = RBS::Patch.new
-      p.apply2(path: Pathname("#{__dir__}/../files/dir_a"))
-      p.apply2(path: Pathname("#{__dir__}/../files/dir_b"))
+      p.apply(path: Pathname("#{__dir__}/../files/dir_a"))
+      p.apply(path: Pathname("#{__dir__}/../files/dir_b"))
 
       assert_equal(<<~EXPECTED, p.to_s)
         class A
@@ -192,7 +192,7 @@ module RBS
 
     def test_nested_module_structure
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         module M_OUTER
           module M_INNER
             class A
@@ -215,13 +215,13 @@ module RBS
 
     def test_overrides_class_with_annotation
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
           def b: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         %a{patch:override}
         class A
           def a: (untyped) -> untyped
@@ -239,7 +239,7 @@ module RBS
 
     def test_deletes_class
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
         end
@@ -247,7 +247,7 @@ module RBS
           def b: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         %a{patch:delete}
         class A
         end
@@ -262,7 +262,7 @@ module RBS
 
     def test_inserts_class_after_specific_class
       p = RBS::Patch.new
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         class A
           def a: () -> void
         end
@@ -273,7 +273,7 @@ module RBS
           def c: () -> void
         end
       RBS
-      p.apply2(<<~RBS)
+      p.apply(<<~RBS)
         %a{patch:append_after:A}
         class D
           def d: () -> void
